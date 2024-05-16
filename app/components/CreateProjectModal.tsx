ee61b9fd40e4c0eab9b2fb5e33dcf31e7192c13a
유일 <83910706+minumsa@users.noreply.github.com>
2024-05-16 16:00:52 +0900

    feat: 프로젝트 생성 모달 name, onthology input 블록 생성

--- app/components/CreateProjectModal.tsx
+++ app/components/CreateProjectModal.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSetAtom } from "jotai";
 import styles from "./CreateProjectModal.module.css";
 import { showCreateModalAtom } from "../modules/atoms";
 
 export const CreateProjectModal = () => {
   const setShowCreateModal = useSetAtom(showCreateModalAtom);
+  const [name, setName] = useState<string>("");
+  const [currentOntology, setCurrentOntology] = useState("34955997_test_ontology");
 
   const handleSave = () => {
     console.log("Save clicked");
@@ -14,9 +16,27 @@ export const CreateProjectModal = () => {
     setShowCreateModal(false);
   };
 
+  const handleSelectOntology = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentOntology(e.target.value);
+  };
+
   return (
     <div className={styles.dimWrapper}>
       <div className={styles.createProjectContainer}>
+        <div className={styles.inputBlockWrapper}>
+          <div>Name</div>
+          <input
+            className={styles.input}
+            placeholder="create a new project by typing its name and pressing Enter"
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputBlockWrapper}>
+          <div>Ontology</div>
+          <select className={styles.input} onChange={handleSelectOntology}>
+            <option value="34955997_test_ontology">34955997_test_ontology</option>
+          </select>
+        </div>
         <div className={styles.modalActions}>
           <button onClick={handleCancel} className={styles.buttonCancel}>
             Cancel

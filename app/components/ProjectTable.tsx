5f1047c5355bf1fc90258b02f70f9e1914f1a48a
유일 <83910706+minumsa@users.noreply.github.com>
2024-05-16 13:52:10 +0900

    feat: 테이블 height를 고정시키고 페이지당 보여줄 아이템 수를 10개로 고정하는 방식으로 페이지네이션 구현

--- app/components/ProjectTable.tsx
+++ app/components/ProjectTable.tsx
@@ -10,50 +10,73 @@ interface ProjectTableProps {
 
 export const ProjectTable = ({ data }: ProjectTableProps) => {
   const [selectedProjectId, setSelectedProjectId] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   const handleRowClick = (projectId: number) => {
     setSelectedProjectId(selectedProjectId === projectId ? null : projectId);
   };
 
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const paginatedData = data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Project</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>OnthologyName</th>
-            <th>Number of Images</th>
-            <th>Created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((project, index) => (
-            <>
-              <tr key={index} onClick={() => handleRowClick(project.id)}>
-                <td>{project.id}</td>
-                <td>{project.name}</td>
-                <td>{project.ontologyName}</td>
-                <td>{project.numberOfImages}</td>
-                <td>{project.created}</td>
-              </tr>
-              {selectedProjectId === project.id && (
-                <tr key={"new"}>
-                  <td colSpan={5}>
-                    <h2>Selected Project Details</h2>
-                    <p>Id: {project.id}</p>
-                    <p>Name: {project.name}</p>
-                    <p>OnthologyName: {project.ontologyName}</p>
-                    <p>Number of Images: {project.numberOfImages}</p>
-                    <p>Created: {project.created}</p>
-                  </td>
+      <div className={styles.tableContainer}>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>OnthologyName</th>
+              <th>Number of Images</th>
+              <th>Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedData.map((project, index) => (
+              <>
+                <tr key={index} onClick={() => handleRowClick(project.id)}>
+                  <td>{project.id}</td>
+                  <td>{project.name}</td>
+                  <td>{project.ontologyName}</td>
+                  <td>{project.numberOfImages}</td>
+                  <td>{project.created}</td>
                 </tr>
-              )}
-            </>
-          ))}
-        </tbody>
-      </table>
+                {selectedProjectId === project.id && (
+                  <tr key={"new"}>
+                    <td colSpan={5}>
+                      <h2>Selected Project Details</h2>
+                      <p>Id: {project.id}</p>
+                      <p>Name: {project.name}</p>
+                      <p>OnthologyName: {project.ontologyName}</p>
+                      <p>Number of Images: {project.numberOfImages}</p>
+                      <p>Created: {project.created}</p>
+                    </td>
+                  </tr>
+                )}
+              </>
+            ))}
+          </tbody>
+        </table>
+        <div className={styles.pagination}>
+          {Array.from({ length: Math.ceil(data.length / itemsPerPage) }, (_, i) => i + 1).map(
+            pageNumber => (
+              <button
+                className={styles.button}
+                key={pageNumber}
+                onClick={() => handlePageChange(pageNumber)}
+              >
+                {pageNumber}
+              </button>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
